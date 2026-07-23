d257cdaab01958ba3bac6e10768c91daf9b8cbf0
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-07-23 03:20:42 +0000

    Manage to fix the scope issue with the for of loop within the function, by just initializing the num variable outside of the for of loop block, at the biginning of the function. After finishing out the loop of summing the age elements in the new array, the sum is then divided by the array length, and the result is rounded up with the Math.round() method. This solves the assignment

--- Get-Average-Age/script.js
+++ Get-Average-Age/script.js
@@ -7,14 +7,14 @@ let arr = [ pete, john, mary ];
 // formula for the average is (age1 + age2 + ... + ageN) / N
 
 function getAverageAge(arr) {
+    let num = 0;
     let arrCopy = arr.map((ele) => {let eleCopy = ele.age; return eleCopy;});
     arrCopy.sort((a, b) => a - b);
     console.log(arrCopy);
     for(const elem of arrCopy) {
-        let num = 0;
         num = num + elem;
         console.log(num);
-    }; return num / (arrCopy.length - 1);
+    }; return Math.round(num / (arrCopy.length));
 }
 
 console.log(getAverageAge(arr));
