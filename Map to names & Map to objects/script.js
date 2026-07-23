a6d022b1945019a585df0163dc66995cfd04e1ee
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-07-23 00:47:00 +0000

    Resolve the problem for Map to objects by using the map method to iterate on the initial array of objects, to then return a copy with a new property that combines with a string literal the element proberty name and surname in a single fullName property. For the map to names the solution I found was similar but consisted more in returning only the element porperty name by refericing into another variable within the map method and returning the variable

--- Map to names & Map to objects/script.js
+++ Map to names & Map to objects/script.js
@@ -1,3 +1,5 @@
+// Map to names
+
 let john = { name: "John", age: 25, };
 let pete = { name: "Pete", age: 30, };
 let mary = { name: "Mary", age: 28, };
@@ -8,3 +10,16 @@ let names = users.map((ele) => {let copyArr = ele.name; return copyArr;});
 
 console.log(names);
 console.log(john);
+
+
+//Map to objects
+
+let john2 = { name: "John", surname: "Smith", id: 1, };
+let pete2 = { name: "Pete", surname: "Hunt", id: 2 };
+let mary2 = { name: "Mary", surname: "Key", id: 3} ;
+
+let users2 = [ john2, pete2, mary2 ];
+
+let usersMapped = users2.map((ele) => {let copyObj = {fullName: `${ele.name} ${ele.surname}`, id: ele.id}; return copyObj;});
+
+console.log(usersMapped);
